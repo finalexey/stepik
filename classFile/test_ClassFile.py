92c056f3a5b32f5a5d8398f7765ea393f25cf91a
Alexey <55698534+finalexey@users.noreply.github.com>
2020-01-16 23:48:24 +0300

    Update test_ClassFile.py

--- classFile/test_ClassFile.py
+++ classFile/test_ClassFile.py
@@ -7,13 +7,16 @@ class TestClassFile(unittest.TestCase):
 
     def setUp(self):
         self.file = ClassFile.File('333.txt')
-        f = open('333.txt', 'w')
-        f.close()
+
 
     def test_write(self, line='sample'):
         self.file.write(line)
         for text in self.file:
             self.assertEqual(line, text)
+            
+    def tearDown(self):
+        f = open('333.txt', 'w')
+        f.close()
 
 
 if __name__ == '__main__':
